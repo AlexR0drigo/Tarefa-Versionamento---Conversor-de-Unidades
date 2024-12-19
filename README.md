b5944b3d7a40ab3507aabdcdb1bd90315dbfddf2
AlexR0drigo <alexrodrigopbdev@gmail.com>
2024-12-18 23:05:53 -0300

    Criado interface inicial
    A interface inicial foi criada com cases de exemplo dentro de uma estrutura de repetição que simula um menu

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Tarefa-Versionamento-Conversor-de-Unidades
-Bem-vindo ao Conversor de Unidades, um projeto colaborativo desenvolvido para consolidar conceitos de versionamento e uso de Git/GitHub. Este programa é implementado em linguagem C e permite a conversão de diferentes tipos de unidades, como comprimento, massa, temperatura e muito mais.
+Bem-vindo ao Conversor de Unidades, um projeto colaborativo desenvolvido para consolidar conceitos de versionamento e uso de Git/GitHub. Este programa é implementado em linguagem C e permite a conversão de diferentes tipos de unidades, como...
--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int opcao;
+
+    do {
+
+        printf("\nMenu de opcoes:\n");
+        printf("1 - Opcao 1\n");
+        printf("2 - Opcao 2\n");
+        printf("3 - Opcao 3\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+
+        switch (opcao) {
+            case 1:
+                printf("Voce escolheu a Opcao 1.\n");
+                break;
+
+            case 2:
+                printf("Voce escolheu a Opcao 2.\n");
+                break;
+
+            case 3:
+                printf("Voce escolheu a Opcao 3.\n");
+                break;
+
+            case 0:
+                printf("Saindo do programa...\n");
+                break;
+
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+                break;
+        }
+    } while (opcao != 0); 
+
     return 0;
 }
